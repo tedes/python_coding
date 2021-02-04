f74d988ff1a1baafd3aa580b49cb911706cd032e
MICHELE MANGIACOTTI <mangiacotti89michele@gmail.com>
2021-02-04 12:53:18 +0100

    added is_unique function

--- myArraysStringFunctions.py
+++ myArraysStringFunctions.py
@@ -1,4 +1,18 @@
 
 
 def is_unique(s):
+    for i in range(0,len(s)-1):
+        for j in range(i+1,len(s)):
+            if s[i] == s[j]:
+                return False
+    return True
+        
 
+
+def main():
+    print(is_unique("asdff"))
+
+
+
+if __name__ == "__main__":
+    main()
