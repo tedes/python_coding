6bf941b1e6176f26d7cbd73be0afa6c7f5ccbe79
mangiacotti michele <mangiacotti89michele@gmail.com>
2021-02-21 14:05:35 +0100

    inizializza linked list

--- myLinkedListFunctions.py
+++ myLinkedListFunctions.py
@@ -1,14 +1,16 @@
 import collections
 
-
-
-
+def fill_linked_list(linked_list):
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
 
 
 
 def main():
-    print(is_unique("asdff"))
-
+   my_linked_list = collections.deque()
+   fill_linked_list(my_linked_list)
+   print(my_linked_list)
 
 
 if __name__ == "__main__":
